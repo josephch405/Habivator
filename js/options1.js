a320a7ce2af9887b91beeac5dc94320f76f764d0
josephch405 <josephch405@gmail.com>
2014-12-01 06:27:16 +0800

    Start graph implementation

---
+++ js/options1.js
@@ -0,0 +1,50 @@
+//imports from localStorage
+importData();
+
+archGroupArray.sort(function(a,b){return Date.parse(b.date)-Date.parse(a.date)})
+
+//shuffle at start of day?
+
+document.body.style.width=600*archGroupArray.length;
+//set page dimensions
+
+var faceButton = document.getElementById("face");
+faceButton.onmouseover = function() {
+    faceButton.src = '../img/face/smile.png';
+}
+faceButton.onmouseout = function() {
+    faceButton.src = '../img/face/up.png';
+}
+faceButton.onclick = smileyToggle;
+var popBox = document.getElementById("popBox");
+popBox.onclick = smileyKeepOn;
+document.body.onclick = smileyOff;
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+var ctx = document.getElementById("chart1").getContext("2d");
+var chart1 = new Chart(ctx).Line(data);
